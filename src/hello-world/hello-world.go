e15156bddd735a5b9843b753e9979a1b883172e4
karki011 <karki011@gmail.com>
2019-03-10 21:12:47 -0500

    .

--- src/hello-world/hello-world.go
+++ src/hello-world/hello-world.go
@@ -6,9 +6,12 @@ import (
 )
 
 func main() {
+
+	//route handler below
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
+	//----------
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
 
